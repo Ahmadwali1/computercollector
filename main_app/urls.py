8f562d1efdbd60ad21be58cd494c82f0c773bbfd
Ahmadwali1 <walia3894@gmail.com>
2023-11-17 23:59:27 +0500

    create computer class in models.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from.import views
 
 
 urlpatterns = [
-  path('', views.home, name='home')
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('computers/', views.computers_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+# computers = [
+#   {'name': 'macbook m2 14 inch', 'storage': '500 GB', 'Color': 'black', 'condition': 'new'},
+#   {'name': 'macbook m3 16 inch', 'storage': '1 TB', 'Color': 'white', 'condition': 'old'},
+# ]
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def computers_index(request):
+  return render(request, 'computers/index.html', {
+    'computers': computers
+  })
